6acce89081afe56858ec3b18efc24d5075776e9f
Artem Batalov <a@batalov.me>
2020-01-17 01:12:11 +0700

    First web

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify
+from providers import rutracker, rutor
+from torrent import Torrents
+
+from multiprocessing.pool import ThreadPool
+pool = ThreadPool(processes=8)
 
 app = Flask(__name__)
 
+tasks = {}
+tasks_ai = 0
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/api/search/<query>')
+def make_task(query):
+    global tasks_ai
+    tasks[tasks_ai] = pool.apply_async(search, [query])
+    tasks_ai += 1
+    return str(tasks_ai - 1)
+
+@app.route('/api/task/<int:id>')
+def status(id):
+    if tasks[id].ready():
+        return jsonify(tasks[id].get())
+    else:
+        return 'Not ready'
+
+
+def search(query, *args, **kwards):
+    print('started')
+    found = []
+    found += rutracker.search(query)
+    found += rutor.search(query)
+
+    tor = Torrents(found)
+
+    return list(map(dict, tor))
 
 
 if __name__ == '__main__':
---
+++ providers.py
@@ -0,0 +1,43 @@
+from provider import Provider
+
+rutracker = Provider(domain='https://rutracker.org/forum/',
+                     auth={
+                         'request': {'url': 'login.php',
+                                     'login_field': 'login_username',
+                                     'password_field': 'login_password',
+                                     'params': {'login': 'Login'}},
+                         'login': 'LOGIN',
+                         'password': 'PASSWORD'
+                     },
+                     search={
+                         'request': {'url_template': 'tracker.php?nm={}&prev_new=0&prev_oop=1&f[]=-1&o=10&s=2&oop=1&pn='},
+                         'pages': {'selector': '.bottom_info .pg',
+                                   'start': 0,
+                                   'end': -1},
+                         'rows': '#tor-tbl > tbody > tr',
+                         'name': 'td.t-title a',
+                         'size': {'selector': 'td.tor-size',
+                                  'attribute': 'data-ts_text',
+                                  'num': 0},
+                         'link': 'td.tor-size a',
+                         'magnet': None,
+                         'seeds': '.seedmed',
+                         'leeches': '.leechmed'
+                     }) 
+
+rutor = Provider(domain='http://rutor.info/',
+                 search={
+                     'request': {'url_template': 'search/0/0/300/2/{}'},
+                     'pages': {'selector': '.bottom_info .pg',
+                               'start': 0,
+                               'end': -1},
+                     'rows': '#index .gai, #index .tum',
+                     'name': 'a[href*=torrent]',
+                     'size': {'selector': 'td[align=right]',
+                              'attribute': None,
+                              'num': -1},
+                     'link': 'a.downgif',
+                     'magnet': 'a[href*=magnet]',
+                     'seeds': '.green',
+                     'leeches': '.red'
+                 })
